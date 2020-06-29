246984518822eec3dcfde12c3f6fc34a5a41adcc
Judy Naegeli <jnaegeli@gmail.com>
2020-06-28 18:48:47 -0700

    Everything works except for the display of the final result. rework the Math.random code

--- script.js
+++ script.js
@@ -15,42 +15,56 @@ function userInput() {
     var spChar = confirm("Would you like to include special characters in your password?");
     var lcChar = confirm("Would you like to include lowercase letters in your password?");
     var ucChar = confirm("Would you like to include uppercase letters in your password?");
-    var availChar = []
-
-    if (numChar) {
-        availChar.push(numbers)
-    } 
-
-    if (spChar) {
-        availChar.push(specials)
-    }
-
-    if (lcChar) {
-        availChar.push(lcLetters)
-    }
-
-    if (ucChar) {
-        availChar.push(ucLetters)
-    }
-
+    
     console.log(numChar);
     console.log(spChar);
     console.log(lcChar);
     console.log(ucChar);
+
+    const lcLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const ucLetters = ['A', 'B', 'C', 'D', 'E', 'F' ,'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const specials = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '`', '{', '|', '}', '~'];
+    
+    let availChar = []
+        
+    if (numChar) {
+        availChar.push(...numbers);
+    } 
+    
+    if (spChar) {
+        availChar.push(...specials);
+    }
+    
+    if (lcChar) {
+        availChar.push(...lcLetters);
+    }
+    
+    if (ucChar) {
+        availChar.push(...ucLetters);
+    }
+    
     console.log(availChar);
 
-    document.getElementById("password").innerHTML = 5 + 6;
+    function makePassword(howMany) {
+        var result           = '';
+        var charactersLength = availChar.length;
+        for ( var i = 0; i < howMany; i++ ) {
+            result += availChar.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    
+    }
+
+    document.getElementById("password").innerHTML = result;
+
+    console.log(result);
 
 }
 
-const lcLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const ucLetters = ['A', 'B', 'C', 'D', 'E', 'F' ,'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const specials = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '`', '{', '|', '}', '~'];
 
-const randomCharacter = lcLetters[Math.floor(Math.random() * lcLetters.length)];
 
-console.log(randomCharacter);
+
 
 // Definition of perimeters: numbers, lowercase letters, uppercase letters, special characters
 // Limit to user selections: which sets can we draw from?
