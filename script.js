b2b3607e7760cd34748f5d6dfc7651ac339d6cbf
Judy Naegeli <jnaegeli@gmail.com>
2020-06-28 21:04:28 -0700

    final product, updated functional code

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const specials = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '`', '{', '|', '}', '~'];
+const lcLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const ucLetters = ['A', 'B', 'C', 'D', 'E', 'F' ,'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 function userInput() {
     var howMany = prompt("How many characters would you like in your password?");
 
@@ -7,24 +12,22 @@ function userInput() {
         var howMany = prompt("How many characters would you like in your password?");
     }
 
-    if (howMany > 7 || howMany < 129) {
-        console.log(howMany);
-    }
-
     var numChar = confirm("Would you like to include numbers in your password?");
     var spChar = confirm("Would you like to include special characters in your password?");
     var lcChar = confirm("Would you like to include lowercase letters in your password?");
     var ucChar = confirm("Would you like to include uppercase letters in your password?");
-    
-    console.log(numChar);
-    console.log(spChar);
-    console.log(lcChar);
-    console.log(ucChar);
 
-    const lcLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    const ucLetters = ['A', 'B', 'C', 'D', 'E', 'F' ,'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const specials = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '`', '{', '|', '}', '~'];
+    while (numChar === false && spChar === false && lcChar ===false && ucChar ===false) {
+        alert("Select at least one type of character for your password.");
+        delete numChar;
+        delete spChar;
+        delete lcChar;
+        delete ucChar;
+        var numChar = confirm("Would you like to include numbers in your password?");
+        var spChar = confirm("Would you like to include special characters in your password?");
+        var lcChar = confirm("Would you like to include lowercase letters in your password?");
+        var ucChar = confirm("Would you like to include uppercase letters in your password?");
+    }
     
     let availChar = []
         
@@ -35,40 +38,25 @@ function userInput() {
     if (spChar) {
         availChar.push(...specials);
     }
-    
+
     if (lcChar) {
         availChar.push(...lcLetters);
     }
-    
+
     if (ucChar) {
         availChar.push(...ucLetters);
     }
     
-    console.log(availChar);
-
-    function makePassword(howMany) {
-        var result           = '';
-        var charactersLength = availChar.length;
-        for ( var i = 0; i < howMany; i++ ) {
-            result += availChar.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
-    
-    }
-
-    document.getElementById("password").innerHTML = result;
-
-    console.log(result);
-
+    document.getElementById("password").innerHTML = makePassword(howMany, availChar);
 }
 
+function makePassword(howMany, availChar) {
+    var text = "";
+    
+    for (var i = 0; i < howMany; i++) {
+        var j = Math.floor(Math.random() * availChar.length);
+        text += availChar[j];
+    }
 
-
-
-
-// Definition of perimeters: numbers, lowercase letters, uppercase letters, special characters
-// Limit to user selections: which sets can we draw from?
-// Draw random characters out of complete set of available characters based on:
-// 1. user set selections
-// 2. set perimeters
-// 3. user input, number of characters
+    return text;
+}
